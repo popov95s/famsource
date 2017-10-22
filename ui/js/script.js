f0e4c20a20af474f7ca07b863e9c7cdd3640e2d0
Jayant <killingsoftlyrbk@gmail.com>
2017-10-22 10:16:10 -0500

    change3

--- ui/js/script.js
+++ ui/js/script.js
@@ -8,6 +8,11 @@ var options = [
   $(document).ready(function() {
     $('select').material_select();
     $('.modal').modal();
+
+    $('.alert-close').on('click', function(c){
+		$(this).parent().parent().parent().parent().fadeOut('slow', function(c){
+		});
+	});	
   });
   
            
