aaec9ed221976d0c80c4b2b601a09e74bfd6a620
Jayant <killingsoftlyrbk@gmail.com>
2017-10-22 02:12:15 -0500

    Join Family Complete

---
+++ ui/js/script.js
@@ -0,0 +1,6 @@
+var options = [
+    {selector: '#staggered-test', offset: 400, callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    } }
+  ];
+  Materialize.scrollFire(options);
